e6239c0c29466a36a9ff8e2ae553933c640217ad
s-glocknera <s-glocknera@52S500037469.it.bsd405.org>
2018-12-03 12:10:31 -0800

    Soon my Robot can go as fast as I can down I-90!

--- src/org/usfirst/frc/team1/robot/IntakeInOutSubsystem.java
+++ src/org/usfirst/frc/team1/robot/IntakeInOutSubsystem.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team1.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class IntakeInOutSubsystem extends Subsystem{
@@ -8,7 +10,8 @@ public class IntakeInOutSubsystem extends Subsystem{
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
 	}
-	public void intakeIn() {}
-public void intakeOut() {}
-public void intakeStop() {}
+public void intakeIn() {roller.set(1.0);}
+public void intakeOut() {roller.set(-1.0);}
+public void intakeStop() {roller.set(0.0);}
+private WPI_TalonSRX roller = RobotMap.roller;
 }
